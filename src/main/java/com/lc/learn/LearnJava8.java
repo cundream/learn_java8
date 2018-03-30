305407fd908b6b42dcb7b32114f69a2b4990107d
lic <cundream@163.com>
2018-03-30 15:06:49 +0800

    Continue to update

--- src/main/java/com/lc/learn/LearnJava8.java
+++ src/main/java/com/lc/learn/LearnJava8.java
@@ -1,10 +1,18 @@
 package com.lc.learn;
 
+import java.lang.reflect.Array;
+import java.util.AbstractMap;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.OptionalInt;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.function.IntBinaryOperator;
+import java.util.function.IntFunction;
 import java.util.function.IntPredicate;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -146,49 +154,324 @@ public class LearnJava8 {
 
     /**
      * 移除数组中的元素，直到传递的函数返回true。返回数组中剩余的元素。
-     * @param elements
+     * @param arrays
      * @param condition  IntPredicate 表示一个整数值参数谓词（布尔值函数）
      * @return
      */
-    public static int[] dropElements(int[] elements, IntPredicate condition){
+    public static int[] dropElements(int[] arrays, IntPredicate condition){
         //Arrays.copyOfRange(T[ ] original,int from,int to)
         // 将一个原始的数组original，从小标from开始复制，复制到小标to，生成一个新的数组。
         // 注意这里包括下标from，不包括下标to。
-        while(elements.length > 0 && !condition.test(elements[0])){
-            elements = Arrays.copyOfRange(elements,1,elements.length);
+        while(arrays.length > 0 && !condition.test(arrays[0])){
+            arrays = Arrays.copyOfRange(arrays,1,arrays.length);
         }
-        return elements;
+        return arrays;
 
     }
 
     /**
      * 返回一个从右边移除n个元素的新数组。
-     * @param elements
+     * @param arrays
      * @param n
      * @return
      */
-    public static int[] dropRight(int[] elements, int n){
+    public static int[] dropRight(int[] arrays, int n){
         if(n < 0){
             throw new IllegalArgumentException("n is less than 0");
         }
-        return n < elements.length
-                ?Arrays.copyOfRange(elements,0,elements.length - n)
+        return n < arrays.length
+                ?Arrays.copyOfRange(arrays,0,arrays.length - n)
                 :new int[0];
     }
 
 
     /**
-     *
-     * @param elements
+     * 返回数组中每个第 nth 个元素（例如：1,2,4,3   n= 2   返回   2,3）
+     * @param arrays
      * @param nth
      * @return
      */
-    public static int[] everyNth(int[] elements, int nth) {
-        return IntStream.range(0, elements.length)
+    public static int[] everyNth(int[] arrays, int nth) {
+        return IntStream.range(0, arrays.length)
                 .filter(i ->  i % nth == nth - 1)
-                .map(i -> elements[i])
+                .map(i -> arrays[i])
                 .toArray();
     }
+
+
+    /**
+     * 查找数组中元素的索引。不存在返回-1
+     * @param arrays
+     * @param el
+     * @return
+     */
+    public static int indexOf(int[] arrays, int el) {
+        return IntStream.range(0, arrays.length)
+                .filter(i -> arrays[i] == el)
+                .findFirst()
+                .orElse(-1);
+    }
+
+
+    /**
+     *查找数组中元素出现的最后一个索引。不存在返回-1
+     * @param arrays
+     * @param el
+     * @return
+     */
+    public static int lastIndexOf(int[] arrays, int el) {
+        return IntStream.iterate(arrays.length - 1, i -> i - 1)
+                .limit(arrays.length)
+                .filter(idx -> arrays[idx] == el)
+                .findFirst()
+                .orElse(-1);
+    }
+
+
+    /**
+     * 过滤数组中非唯一的数值
+     * @param arrays
+     * @return  数组中为唯一的值
+     */
+    public static int[] filterNonUnique(int[] arrays){
+        return Arrays.stream(arrays)
+                .filter(el -> indexOf(arrays, el) == lastIndexOf(arrays, el))
+                .toArray();
+    }
+
+
+    /**
+     * 返回一个数组
+     * @param arrays
+     * @return
+     */
+    public static int[] flatten(Object[] arrays){
+        return Arrays.stream(arrays).flatMapToInt(a -> a instanceof int[]
+                ? Arrays.stream((int[]) a)
+                : IntStream.of((int)a)
+                ).toArray();
+    }
+
+
+    /**
+     *  根据给定函数对数组元素进行分组。
+     * @param arrays
+     * @param func
+     * @param <T>
+     * @param <R>
+     * @return
+     */
+    public static <T , R> Map<R , List<T>> groupBy(T[] arrays , Function<T , R> func){
+
+        return Arrays.stream(arrays).collect(Collectors.groupingBy(func));
+    }
+
+
+    /**
+     * 返回除数组最后一个元素外的所有元素
+     * @param arrays
+     * @param <T>
+     * @return
+     */
+    public static <T> T[] initial (T[] arrays){
+        return Arrays.copyOfRange(arrays,0,arrays.length - 1);
+    }
+
+
+    /**
+     * 初始化包含指定范围内的数字的数组，其中起始和结束都是包含的
+     * @param start
+     * @param end
+     * @return
+     */
+    public static int[] initializeArrayWithRange(int start,int end ){
+
+        return IntStream.rangeClosed(start,end).toArray();
+
+    }
+
+    /**
+     * 初始化 n 个 值为 value 的数组
+     * @param value
+     * @param n
+     * @return
+     */
+    public static int[] initializeArrayWithValues(int value , int n){
+        return  IntStream.generate(() -> value).limit(n).toArray();
+    }
+
+    /**
+     * 返回两个数组相同值
+     * @param first
+     * @param second
+     * @return
+     */
+    public static int[] intersection(int[] first,int[] second){
+        Set<Integer> set = Arrays.stream(second).boxed().collect(Collectors.toSet());
+        return Arrays.stream(first)
+                .filter(set::contains)
+                .toArray();
+    }
+
+
+    /**
+     * 返回数组是否是排序的   正序 1 ，反序 -1 ，无序 0
+     * @param arr
+     * @param <T>
+     * @return
+     */
+
+    public static <T extends Comparable<? super T>> int isSorted(T[] arr) {
+        final int direction = arr[0].compareTo(arr[1]) < 0 ? 1 : -1;
+        for (int i = 0; i < arr.length; i++) {
+            T val = arr[i];
+            if (i == arr.length - 1) return direction;
+            else if ((val.compareTo(arr[i + 1]) * direction > 0)) return 0;
+        }
+        return direction;
+    }
+
+
+    /**
+     * 把数组内的元素拼接字符串
+     * @param arr
+     * @param separator 分隔符号
+     * @param end  最后两个的拼接符号
+     * @param <T>
+     * @return
+     */
+    public static <T> String join(T[] arr, String separator, String end) {
+        return IntStream.range(0, arr.length)
+                .mapToObj(i -> new AbstractMap.SimpleEntry<>(i, arr[i]))
+                .reduce("", (acc, val) -> val.getKey() == arr.length - 2
+                        ? acc + val.getValue() + end
+                        : val.getKey() == arr.length - 1 ? acc + val.getValue() : acc + val.getValue() + separator, (fst, snd) -> fst);
+    }
+
+
+    /**
+     * 指定分隔符号
+     * @param arr
+     * @param separator
+     * @param <T>
+     * @return
+     */
+    public static <T> String join(T[] arr, String separator) {
+        return join(arr, separator, separator);
+    }
+
+    /**
+     * 只分隔
+     * @param arr
+     * @param <T>
+     * @return
+     */
+    public static <T> String join(T[] arr) {
+        return join(arr, ",");
+    }
+
+
+    /**
+     * 取数组的 下标 为n  个元素  n > 0 取下标为n ，n < 0  从未向前
+     * @param arr
+     * @param n
+     * @param <T>
+     * @return
+     */
+    public static <T> T nthElement(T[] arr, int n) {
+        if (n > 0) {
+            return Arrays.copyOfRange(arr, n, arr.length)[0];
+        }
+        return Arrays.copyOfRange(arr, arr.length + n, arr.length)[0];
+    }
+
+
+    /**
+     * 选择键对应的键对
+     * @param obj
+     * @param arr
+     * @param <T>
+     * @param <R>
+     * @return
+     */
+    public static <T, R> Map<T, R> pick(Map<T, R> obj, T[] arr) {
+        return Arrays.stream(arr)
+                .filter(obj::containsKey)
+                .collect(Collectors.toMap(k -> k, obj::get));
+    }
+
+
+    /**
+     * 根据函数过滤 Map数组中，key在keys数组中的元素
+     * @param data
+     * @param keys
+     * @param fn
+     * @return
+     */
+    public static Map<String, Object>[] reducedFilter(Map<String, Object>[] data, String[] keys, Predicate<Map<String, Object>> fn) {
+        return Arrays.stream(data)
+                .filter(fn)
+                .map(el -> Arrays.stream(keys).filter(el::containsKey)
+                        .collect(Collectors.toMap(Function.identity(), el::get)))
+                .toArray((IntFunction<Map<String, Object>[]>) Map[]::new);
+    }
+
+    /**
+     *随机获取数组中的元素
+     * @param arr
+     * @param <T>
+     * @return
+     */
+    public static <T> T sample(T[] arr) {
+        return arr[(int) Math.floor(Math.random() * arr.length)];
+    }
+
+
+    /**
+     * 随机返回数组中指定个数的元素
+     * @param input
+     * @param n
+     * @param <T>
+     * @return
+     */
+    public static <T> T[] sampleSize(T[] input, int n) {
+        T[] arr = Arrays.copyOf(input, input.length);
+        int length = arr.length;
+        int m = length;
+        while (m > 0) {
+            int i = (int) Math.floor(Math.random() * m--);
+            T tmp = arr[i];
+            arr[i] = arr[m];
+            arr[m] = tmp;
+        }
+        return Arrays.copyOfRange(arr, 0, n > length ? length : n);
+    }
+
+
+    /**
+     * 随机排序数组
+     * @param input
+     * @param <T>
+     * @return
+     */
+    public static <T> T[] shuffle(T[] input) {
+        T[] arr = Arrays.copyOf(input, input.length);
+        int length = arr.length;
+        int m = length;
+        while (m > 0) {
+            int i = (int) Math.floor(Math.random() * m--);
+            T tmp = arr[i];
+            arr[i] = arr[m];
+            arr[m] = tmp;
+        }
+        return arr;
+    }
+
+    public static <T> T[] emptyArray(Class<T> clz) {
+        return (T[]) Array.newInstance(clz, 0);
+    }
+
+
 
 
 
--- src/test/java/com/lc/leran/TestLearnJava8.java
+++ src/test/java/com/lc/leran/TestLearnJava8.java
@@ -3,7 +3,11 @@ package com.lc.leran;
 import com.lc.learn.LearnJava8;
 import org.junit.Test;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.OptionalInt;
 import java.util.stream.IntStream;
 
@@ -16,67 +20,68 @@ import java.util.stream.IntStream;
 public class TestLearnJava8 {
 
     @Test
-    public void test() throws  Exception{}
+    public void test() throws Exception {
+    }
 
     @Test
-    public void gcd_test_1_to_5_is_1(){
-        OptionalInt gcd = LearnJava8.gcd(new int[]{1,2,3,4,5});
+    public void gcd_test_1_to_5_is_1() {
+        OptionalInt gcd = LearnJava8.gcd(new int[]{1, 2, 3, 4, 5});
         print(gcd);
     }
 
     @Test
-    public void gcd_test_4_8_and_12_is_4(){
-        OptionalInt gcd = LearnJava8.gcd(new int[]{4,8,12});
+    public void gcd_test_4_8_and_12_is_4() {
+        OptionalInt gcd = LearnJava8.gcd(new int[]{4, 8, 12});
         print(gcd);
     }
 
     @Test
-    public void lcm_test_4_8_12_is_24() throws  Exception{
-        OptionalInt lcm = LearnJava8.lcm(new int[]{4,8,12});
+    public void lcm_test_4_8_12_is_24() throws Exception {
+        OptionalInt lcm = LearnJava8.lcm(new int[]{4, 8, 12});
         print(lcm);
 
     }
 
     @Test
-    public void max_test() throws  Exception{
-        OptionalInt max = LearnJava8.arrayMax(new int[]{10,1,5,4});
+    public void max_test() throws Exception {
+        OptionalInt max = LearnJava8.arrayMax(new int[]{10, 1, 5, 4});
         print(max);
     }
 
 
     @Test
-    public void min_test() throws  Exception{
-        OptionalInt max = LearnJava8.arrayMin(new int[]{10,1,5,4});
+    public void min_test() throws Exception {
+        OptionalInt max = LearnJava8.arrayMin(new int[]{10, 1, 5, 4});
         print(max);
     }
 
     @Test
     public void chunk_breaks_input_array__with_odd_length() throws Exception {
         int[][] chunks = LearnJava8.chunk(new int[]{1, 2, 3, 4, 5}, 2);
-       for (int[] s: chunks){
-           System.out.println("数组大小-----"+s.length);
-           print(s);
-       }
+        for (int[] s : chunks) {
+            System.out.println("数组大小-----" + s.length);
+            print(s);
+        }
     }
 
     @Test
-    public void contOccurrences_test() throws Exception{
+    public void contOccurrences_test() throws Exception {
         long count = LearnJava8.contOccurrences(new int[]{1, 1, 2, 1, 2, 3}, 1);
         System.out.println(count);
     }
 
 
     @Test
-    public void deepFlatten_test()throws Exception{
-        int[] flatten = LearnJava8.deepFlatten(new Object[]{1,new Object[]{2},new Object[]{3,4,5}});
+    public void deepFlatten_test() throws Exception {
+        int[] flatten = LearnJava8.deepFlatten(new Object[]{1, new Object[]{2}, new Object[]{3, 4, 5}});
         print(flatten);
 
     }
 
 
     @Test
-    public void difference_test() throws  Exception{
-        int[] difference = LearnJava8.difference(new int[]{1,2,3},new int[]{1,2,4});
+    public void difference_test() throws Exception {
+        int[] difference = LearnJava8.difference(new int[]{1, 2, 3}, new int[]{1, 2, 4});
         print(difference);
     }
 
@@ -93,36 +98,205 @@ public class TestLearnJava8 {
 
 
     @Test
-    public void distinctValuesOfArray_tset() throws  Exception{
-        int[] distinct = LearnJava8.distinctValuesOfArray(new int[]{1,2,2,1,3,4,5});
+    public void distinctValuesOfArray_tset() throws Exception {
+        int[] distinct = LearnJava8.distinctValuesOfArray(new int[]{1, 2, 2, 1, 3, 4, 5});
         print(distinct);
     }
 
     @Test
-    public void dropElements_test() throws  Exception {
-        int[] drop = LearnJava8.dropElements(new int[]{1,2,3,4},i -> i >= 3);
+    public void dropElements_test() throws Exception {
+        int[] drop = LearnJava8.dropElements(new int[]{1, 2, 3, 4}, i -> i >= 3);
         print(drop);
     }
 
     @Test
-    public void dropRight_test() throws  Exception{
-        int[] drop = LearnJava8.dropRight(new int[]{1,2,3,4,5,6,7,8},3);
+    public void dropRight_test() throws Exception {
+        int[] drop = LearnJava8.dropRight(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, 3);
         print(drop);
     }
 
 
     @Test
-    public void everyNth_test() throws  Exception{
-        int[] every = LearnJava8.everyNth(new int[]{1,2,3,4,5,6},3);
+    public void everyNth_test() throws Exception {
+        int[] every = LearnJava8.everyNth(new int[]{1, 2, 4, 3, 5, 6}, 3);
         print(every);
     }
 
+    @Test
+    public void filterNonUnique_test() throws Exception {
+        int[] filter = LearnJava8.filterNonUnique(new int[]{1, 2, 4, 3, 5, 6, 1});
+        print(filter);
+    }
 
-    private void print(int[] arrays){
+
+    @Test
+    public void faltten_test() throws Exception {
+        int[] faltten = LearnJava8.flatten(new Object[]{1, new int[]{1, 2, 3}, 4, 5});
+        print(faltten);
+
+    }
+
+    @Test
+    public void groupBy_test() throws Exception {
+        Map<Integer, List<String>> group = LearnJava8.groupBy(new String[]{"one", "two", "three"}, String::length);
+
+        print(group);
+    }
+
+
+    @Test
+    public void initializeArrayWithRange_test() throws Exception {
+        int[] numbers = LearnJava8.initializeArrayWithRange(1, 5);
+        print(numbers);
+
+    }
+
+    @Test
+    public void initializeArrayWithValues_test() throws Exception {
+        int[] numbers = LearnJava8.initializeArrayWithValues(5, 3);
+        print(numbers);
+    }
+
+
+    @Test
+    public void intersection_test() throws Exception {
+        int[] elements = LearnJava8.intersection(new int[]{7, 1, 2, 3}, new int[]{4, 3, 2, 1, 6, 7, 5, 7});
+        print(elements);
+    }
+
+
+    @Test
+    public void isSorted_test() throws Exception {
+        int t = LearnJava8.isSorted(new Integer[]{1, 2, 3, 7, 8});
+        int t2 = LearnJava8.isSorted(new Integer[]{1, 2, 3, 7, 6});
+        int t3 = LearnJava8.isSorted(new Integer[]{8, 7, 6, 5});
+        int t4 = LearnJava8.isSorted(new Integer[]{8, 5, 6});
+        print(t);
+        print(t2);
+        print(t3);
+        print(t4);
+    }
+
+
+    @Test
+    public void join_different_sep_and_end_test() throws Exception {
+        String joined = LearnJava8.join(new String[]{"pen", "pineapple", "apple", "pensssss", "pensssss"}, ",", "&");
+        print(joined);
+    }
+
+    @Test
+    public void join_with_sep_only_test() throws Exception {
+        String joined = LearnJava8.join(new String[]{"pen", "pineapple", "apple", "pensssss", "pensssss"}, "-");
+        print(joined);
+    }
+
+    @Test
+    public void join_with_default_sep_test() throws Exception {
+        String joined = LearnJava8.join(new String[]{"pen", "pineapple", "apple", "pensssss", "pensssss"});
+        print(joined);
+    }
+
+
+    @Test
+    public void nthElement_positive_sequence_test() throws Exception {
+        String ngElement = LearnJava8.nthElement(new String[]{"a", "b", "c"}, 1);
+        print(ngElement);
+    }
+
+    @Test
+    public void nthElement_reverse_test() throws Exception {
+        String ngElement = LearnJava8.nthElement(new String[]{"a", "b", "c"}, -3);
+        print(ngElement);
+    }
+
+
+    @Test
+    public void pick_test() throws Exception {
+        Map<String, Integer> obj = new HashMap<>();
+        obj.put("a", 1);
+        obj.put("b", 2);
+        obj.put("c", 3);
+        Map<String, Integer> picked = LearnJava8.pick(obj, new String[]{"a", "c"});
+        System.out.println(picked);
+    }
+
+    @Test
+    public void reducedFilter_test() throws Exception {
+        Map<String, Object> item1 = new HashMap<>();
+        item1.put("id", 1);
+        item1.put("name", "john");
+        item1.put("age", 24);
+
+        Map<String, Object> item2 = new HashMap<>();
+        item2.put("id", 2);
+        item2.put("name", "mike");
+        item2.put("age", 50);
+        Map<String, Object> item3 = new HashMap<>();
+        item3.put("id", 3);
+        item3.put("name", "cun");
+        item3.put("age", 45);
+
+        Map<String, Object>[] filtered = LearnJava8.reducedFilter((Map<String, Object>[]) new Map[]{item1, item2, item3}, new String[]{"id", "name", "age"}, item -> (Integer) item.get("age") > 24);
+
+        print(filtered);
+    }
+
+
+    @Test
+    public void sample_random_test() throws Exception {
+
+        Integer sample = LearnJava8.sample(new Integer[]{3, 4, 5, 6, 7, 9, 11});
+        print(sample);
+    }
+
+
+    @Test
+    public void sampleSize_random_than_array_size() throws Exception {
+        Integer[] sample = LearnJava8.sampleSize(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8}, 1);
+
+        print(sample);
+    }
+
+    @Test
+    public void shuffle__test() throws Exception {
+        Integer[] sample = LearnJava8.shuffle(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8});
+
+        print(sample);
+    }
+
+    @Test
+    public void empty_array() throws Exception {
+        String[] empty = LearnJava8.emptyArray(String.class);
+    }
+
+
+
+
+
+
+
+
+
+    private void print(Object[] obj) {
+        for (Object o : obj) {
+            if (o instanceof Object[]) {
+                print((Object[]) o);
+            } else {
+                System.out.println(o);
+            }
+
+        }
+    }
+
+    private void print(int[] arrays) {
         Arrays.stream(arrays).forEach(System.out::println);
     }
 
-    private void print(OptionalInt optionalInt){
+    private void print(Object o) {
+        System.out.println(o);
+    }
+
+    private void print(OptionalInt optionalInt) {
         System.out.println(optionalInt.getAsInt());
     }
 }
